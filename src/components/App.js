beb5b7d5e612e86a022aae382ebd4f76598f795b
sakir tufan <sakirtufan49@gmail.com>
2020-10-15 09:44:07 +0200

    FETCH and AXIOS

--- src/components/App.js
+++ src/components/App.js
@@ -1,47 +1,87 @@
 import React from 'react';
-import SearchBar from './SearchBar';
+
 import MovieList from './MovieList';
+import SearchBar from './SearchBar';
+import axios from 'axios';
 
 
 class App extends React.Component {
   state = {
     movies: [
-      {
-        "id": 1,
-        "name": "The Flash",
-        "rating": 8.3,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/wHa6KOJAoNTFLFtp7wguUJKSnju.jpg"
-      },
 
-      {
-        "id": 2,
-        "name": "Interstellar",
-        "rating": 6.8,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg"
-      },
+    ],
+    searchQuery: ""
+  }
 
-      {
-        "id": 3,
-        "name": "Arrow",
-        "rating": 7.9,
-        "overview": "This is a wider card with supporting text below as a natural lead-in to additional content.",
-        "imageURL": "https://image.tmdb.org/t/p/w220_and_h330_face/gKG5QGz5Ngf8fgWpBsWtlg5L2SF.jpg"
-      }
-    ]
+  //FETCH GET
+  // async componentDidMount() {
+  //   const baseURL = "http://localhost:3002/movies";
+  //   const response = await fetch(baseURL);
+  //   console.log(response);
+  //   const data = await response.json();
+  //   console.log(data);
+  //   this.setState({ movies: data })
+  // }
+
+  //AXIOS GET
+  async componentDidMount(){
+    const response = await axios.get("http://localhost:3002/movies")
+    console.log(response);
+    this.setState({movies:response.data})
+  }
+
+  //FETCH DELETE
+  // deleteMovie =async (movie) => {
+  //   const baseURL=`http://localhost:3002/movies/${movie.id}`
+  //   await fetch(baseURL, {
+  //     method:"DELETE"
+  //   })
+  //   const newMovieList = this.state.movies.filter(
+  //     m => m.id !== movie.id
+  //   );
+
+  //   // this.setState({movies:newMovieList}) ilk state bos oldugu durumlarda
+  //   this.setState(state => ({
+  //     movies: newMovieList
+  //   }))
+  // }
+
+  //AXIOS DELETE
+  deleteMovie = async (movie) => {
+    axios.delete(`http://localhost:3002/movies/${movie.id}`)
+    const newMovieList = this.state.movies.filter(
+      m => m.id !== movie.id
+    );
+
+    // this.setState({movies:newMovieList}) ilk state bos oldugu durumlarda
+    this.setState(state => ({
+      movies: newMovieList
+    }))
+  }
+
+  searchMovie = (event) => {
+    //console.log(event.target.value);
+    this.setState({ searchQuery: event.target.value })
   }
 
   render() {
+
+    let filteredMovies = this.state.movies.filter(
+      (movie) => {
+        return movie.name.toLowerCase().indexOf(this.state.searchQuery.toLowerCase()) !== -1
+      }
+    )
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-lg-12'>
-            <SearchBar/>
+            <SearchBar searchMovieProp={this.searchMovie} />
           </div>
         </div>
 
-        <MovieList/>
+        <MovieList
+          movies={filteredMovies}
+          deleteMovieProp={this.deleteMovie} />
       </div>
     )
   }
